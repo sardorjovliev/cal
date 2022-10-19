fb497f5d10051fd72f2560520c38766e28aaceef
sardorjovliev <115462274+sardorjovliev@users.noreply.github.com>
2022-10-19 15:39:28 +0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,45 @@
+// // number
+// // let a=160;
+// // let b=20;
+// //
+// // string
+// // let name='Anvar';
+// // let name2='Husan';
+// //
+// // window.alert("salom" + name)
+// // console.log(name)
+// console.log(name2)
+// let c=a+b;
+// let d=a*b;
+// console.log(c);
+// console.log(d);
+//
+// let l=40;
+// let b=60;
+// let c=1.6;
+// let h=(a+b)*c;
+// console.log(h)
+// let name='sardor';
+// let name2='anvar';
+// document.getElementById("test").innerHTML='Asalomu alaekum'+"   " +  name +" "+ name2;
+//
+// function test(){
+//     let a,b,c;
+//     a=40;
+//     b=35;
+//     c=a+b;
+//     window.alert(c);
+//     document.getElementById("test3").innerHTML="javob: "+ c;
+// }
+// let a=10;
+// let b=5;
+// let h=(a+b)*(a-b);
+// console.log(h)
+
+function plus(a,b,c) {
+    a=document.getElementById("birson").value;
+    b=document.getElementById("ikison").value;
+    c=parseInt(a)+parseInt(b);
+    document.getElementById("bosing").innerHTML="Javob: " + c;
+
+}
